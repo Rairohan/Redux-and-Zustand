76cb7d1fc849f0e26f5a0a8e8822498da164b09a
rohan <rairohan14303@gmail.com>
2026-09-24 15:26:07 +0545

    bug fixed

--- src/features/TodoTracker.jsx
+++ src/features/TodoTracker.jsx
@@ -16,15 +16,15 @@ export default function TodoTracker(){
                 Add
             </button>
             {
-                todos.map((todo)=>{
-                    return(
+                todos.map((todo)=>
+                    (
                     <p key={todo.id}>
                         {todo.text}
                         <input type="checkbox" checked={todo.completed} onChange={()=>dispatch({type:"toggleTodo",payload:todo.id})} />
                         <button onClick={()=>dispatch({type:"deleteTodo",payload:todo.id})}>Delete</button>
                     </p>
                     )
-                })  
+                )  
             }
         </div>
     )
--- src/store/redux/todosReducer.js
+++ src/store/redux/todosReducer.js
@@ -3,11 +3,11 @@ export default function todosReducer(state=initialState,action){
     switch(action.type){
         case'addTodo': return {todos:[...state.todos,action.payload]}
         case'toggleTodo': return {
-            todos:state.todos.filter(todo=>
+            todos:state.todos.map(todo=>
                 todo.id === action.payload ? {...todo,completed: !todo.completed} : todo
             )
          }
-        case'deleteTodo': return {todo: state.todos.filter(todo => todo.id !== action.payload)}
+        case'deleteTodo': return {todos: state.todos.filter(todo => todo.id !== action.payload)}
         default: return(state)
     }
 }   
